5866cb138e492a0bada70747c04a61425011f868
henryliu <17600047539@163.com>
2024-10-09 22:37:35 +0800

    update

--- apps/web/app/pool/create/page.tsx
+++ apps/web/app/pool/create/page.tsx
@@ -9,12 +9,21 @@ export default function Page() {
   const [PoolSetup,_] = useAtom(PoolSetupOpen);
   if(PoolSetup){
     return (
-      <LiquidityPoolSetup/>
+      <LiquidityPoolSetup selectedTokens={{
+        base: undefined,
+        quote: undefined
+      }} onClose={function (): void {
+        throw new Error("Function not implemented.");
+      } }/>
     )
   }
   else{
     return (
-      <TokenCreate/>
+      <TokenCreate onClose={function (): void {
+        throw new Error("Function not implemented.");
+      } } onSelectTokens={function (base: any, quote: any): void {
+        throw new Error("Function not implemented.");
+      } }/>
     )
   }
 }
