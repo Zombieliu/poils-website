d4f408ba8553bed14a6cbc3ab4e9794ceab156f6
vladilen11 <vladilenaksana@163.com>
2024-11-14 18:46:05 +0800

    update new contract

--- apps/web/app/chain/config.ts
+++ apps/web/app/chain/config.ts
@@ -2,9 +2,9 @@ type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 const NETWORK: NetworkType = 'testnet';
 
-const PACKAGE_ID = '0x4b43678d244e9bd69410631d820069eecace540f3140c4a8e01603173f41d9c5';
-const ASSETS_ID = '0x716ef0c3c4b0c0ff070751ae248c6980ebcfba9a2700205235cbfc190543a155';
-const DEX_ID = '0x32407e1abd816099f80e11d824603d776fe62433a41ee4342fe2f7485043e65d';
-const WRAPPER_ID = '0xc125ce113d392dd9de9c77db9f00613c3cafb0d2a725ace3d0bff71e0fe96ca7';
+const PACKAGE_ID = '0x241f5696abebf5898fb045f5e0a78df974bd9c3c49f99ad9d1639b164b3cf203';
+const ASSETS_ID = '0x29ecbc29ba405bb1f465ad6d52f0feea6fafe1503fadcfdd6fdc1a5a0a755f17';
+const DEX_ID = '0x4ff725dba697072f3a8c66f661a18f8c1d4dc62a4da8c65ebc100ae64acb793a';
+const WRAPPER_ID = '0xb57930689fb1fcc904a8541db0a8e94ed786372876def2400af9d2c80e3491bd';
 
 export { NETWORK, PACKAGE_ID, ASSETS_ID, DEX_ID, WRAPPER_ID };
